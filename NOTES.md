d6d8c572089be3b9839b5786ea8013e5c832430a
Deovrat Singh <devsingh6n@gmail.com>
2020-07-29 22:45:44 +0530

    Added Initial models

---
+++ NOTES.md
@@ -0,0 +1,15 @@
+# Notes for User Flow
+
+
+Product List -Display products
+|
+Product Detail View - Add item to your cart
+
+|
+Cart View - all the items in cart
+|
+Checkout - Address
+|
+Payment 
+|
+Payment successfull - Order Confirmation
--- ecom/urls.py
+++ ecom/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from core import views
@@ -24,7 +24,7 @@ urlpatterns = [
     path('',views.HomeView.as_view(), name ='home'),
     path('contact/',views.ContactView.as_view(), name ='contact'),
     path('admin/', admin.site.urls),
-
+    path('cart/',include('cart.urls', namespace='cart')),
     
 
 ]
