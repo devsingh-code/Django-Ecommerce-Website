c53087e1d31cdf40683686f983eaabe7ff55d752
Deovrat Singh <devsingh6n@gmail.com>
2020-08-16 14:53:24 +0530

    Finalizing rest of staff views

---
+++ staff/forms.py
@@ -0,0 +1,17 @@
+  
+from django import forms
+
+from cart.models import Product
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            'title',
+            'image',
+            'description',
+            'price',
+            'available_colors',
+            'available_size',
+        ]
